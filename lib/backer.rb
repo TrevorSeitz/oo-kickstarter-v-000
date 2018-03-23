d00a25aca947a42913273da9d5c2f225ab816202
Trevor Seitz <trevinla@gmail.com>
2018-03-23 17:54:06 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,7 +2,7 @@ class Backer
   attr_accessor :project, :name
 
   @@add = []
-  
+
   def initialize(name)
     @name = name
   end
--- lib/project.rb
+++ lib/project.rb
@@ -1,3 +1,11 @@
 class Project
 
+attr_accessor :project, :name
+
+@@add = []
+
+def initialize(name)
+  @name = name
+  @backers = []
+end
 end
